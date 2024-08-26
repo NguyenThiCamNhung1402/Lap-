7dfe8f585997f5b587aadaf5eee8b6df8f9eeacd
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-08-26 15:37:33 +0700

    bai5.py

--- bai5.py
+++ bai5.py
@@ -4,8 +4,6 @@ Created on Sun Aug 25 23:36:17 2024
 
 @author: Nguyễn Thị Cẩm Nhung - 23712471
 """
-h =int(input("Nhập giờ:"))
-m =int(input("Nhập phút:"))
-s =int(input("Nhập giây:"))
+h, m, s= map(int, input("Nhập thời gian theo định dạng hh:mm:ss:").split(":"))
 tong =(h *3600)+(m *60)+s 
 print("Tổng số giây:",tong )
